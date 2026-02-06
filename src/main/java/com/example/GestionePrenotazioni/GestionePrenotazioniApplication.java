c6cebef6ba59b4d90ae304a6fee18bd8b7381026
Salvatore <pepesalvatore12@gmail.com>
2026-02-06 11:21:40 +0100

    add empty packages

--- src/main/java/com/example/GestionePrenotazioni/GestionePrenotazioniApplication.java
+++ src/main/java/com/example/GestionePrenotazioni/GestionePrenotazioniApplication.java
@@ -8,6 +8,7 @@ public class GestionePrenotazioniApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionePrenotazioniApplication.class, args);
+        System.out.println("Ciao");
 	}
 
 }
