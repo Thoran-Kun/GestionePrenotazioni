17fc1410d76149a44b99794ef6bc55b7d4392246
Salvatore <pepesalvatore12@gmail.com>
2026-02-07 11:15:48 +0100

    complete test

--- src/main/java/com/example/GestionePrenotazioni/runners/RunnerPrenotazioni.java
+++ src/main/java/com/example/GestionePrenotazioni/runners/RunnerPrenotazioni.java
@@ -5,12 +5,14 @@ import com.example.GestionePrenotazioni.services.EdificioService;
 import com.example.GestionePrenotazioni.services.PostazioneService;
 import com.example.GestionePrenotazioni.services.PrenotazioneService;
 import com.example.GestionePrenotazioni.services.UtenteService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
+@Slf4j
 @Component
 public class RunnerPrenotazioni implements CommandLineRunner {
 
@@ -30,17 +32,58 @@ public class RunnerPrenotazioni implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         //inizio popolando le tabelle nel DB con le mie Entity
-        Edificio edificio = new Edificio("Colosseo", "via Roma Capitale", "Roma");
+        Edificio edificio = new Edificio(
+                "Colosseo",
+                "via Roma Capitale",
+                "Roma");
         edificioService.saveEdificio(edificio);
 
-        Utente utente = new Utente("thor4n_Kun", "SalvatorePepe", "salvo@mail.it");
+        Utente utente = new Utente(
+                "thor4n_Kun",
+                "SalvatorePepe",
+                "salvo@mail.it");
         utenteService.saveUtente(utente);
 
-        Postazione postazione = new Postazione("A001", "Tavolo di lavoro smart", TipoPostazione.OPENSPACE, 4, edificio);
+        Utente utente1 = new Utente(
+                "Spider-man",
+                "PeterParker",
+                "spider@man.com");
+        utenteService.saveUtente(utente1);
+
+        Postazione postazione = new Postazione(
+                "A001",
+                "Tavolo di lavoro smart",
+                TipoPostazione.OPENSPACE,
+                4, edificio);
         postazioneService.savePostazione(postazione);
 
-        Prenotazione prenotazione = new Prenotazione(utente, postazione, LocalDate.now());
+        //testo qualche risultato con la logica e vedo che succede in console
+        LocalDate dataPre = LocalDate.now().plusDays(5);
+
+        log.info("--- TENTATIVO 1: dovrebbe essere valida ---");
+        Prenotazione prenotazione = new Prenotazione(utente, postazione, dataPre);
         prenotazioneService.savePrenotazione(prenotazione);
 
+        log.info("--- TENTATIVO 2: stessa postazione ma con utente diverso (NON deve andare a buon fine) ---");
+        Prenotazione prenotazione1 = new Prenotazione(utente1, postazione, dataPre);
+        prenotazioneService.savePrenotazione(prenotazione1);
+
+        log.info("--- TENTATIVO 3: stesso utente ma postazione diversa, anche questa dovrebbe fallire ---");
+        //per far questo devo creare una nuova postazione di lavoro ma richiamare lo stesso utente
+        Postazione postazione1 = new Postazione(
+                "A002",
+                "sala Riunioni",
+                TipoPostazione.SALA_RIUNIONI,
+                100,
+                edificio);
+        postazioneService.savePostazione(postazione1);
+
+        Prenotazione prenotazione2 = new Prenotazione(utente, postazione1, dataPre);
+        prenotazioneService.savePrenotazione(prenotazione2);
+
+        // ricerco le postazioni
+        log.info("--- RICERCA POSTAZIONI ---");
+        postazioneService.ricercaPostazioni(TipoPostazione.OPENSPACE, "Roma")
+                .forEach(p -> System.out.println("postazione trovata: " + p.getDescrizione()));
     }
 }
--- src/main/java/com/example/GestionePrenotazioni/services/PrenotazioneService.java
+++ src/main/java/com/example/GestionePrenotazioni/services/PrenotazioneService.java
@@ -22,14 +22,44 @@ public class PrenotazioneService {
     public void savePrenotazione(Prenotazione newPrenotazione){
 //        this.prenotazioneRepository.save(newPrenotazione);
 //        log.info("la prenotazione: " + newPrenotazione.toString() + " è stata salvata correttamente nel DB");
-        if(newPrenotazione == null){
-            throw new ValidationException("errore nel salvataggio della prenotazione :(");
+//        if(newPrenotazione == null){
+//            throw new ValidationException("errore nel salvataggio della prenotazione :(");
+//        }
+//        this.prenotazioneRepository.save(newPrenotazione);
+//        log.info("la prenotazione: " + newPrenotazione.toString() + " è stata salvata correttamente nel DB");
+    //1. controllo se la postazione è già occupata per quella data
+        //mi tornerà un booleano true o false in caso. Entro nella postazione Repository e controllo tramite metodo
+        //se la postazione è occupata dando come parametro nella funzione creata nella repository la postazione appunto
+        //e la data.
+        boolean postazioneOccupata = prenotazioneRepository.existsByPostazioneAndDataPrenotazione(
+                newPrenotazione.getPostazione(),newPrenotazione.getDataPrenotazione()
+        );
+
+        if(postazioneOccupata){
+            log.error("impossibile salvare la prenotazione: la postazione {} è già occupata in data {}",
+                  newPrenotazione.getPostazione().getCodiceUnivoco(),
+                  newPrenotazione.getDataPrenotazione());
+            return; // facendo così uscirò dal metodo senza salvare
         }
-        this.prenotazioneRepository.save(newPrenotazione);
-        log.info("la prenotazione: " + newPrenotazione.toString() + " è stata salvata correttamente nel DB");
+
+        //2. la traccia ci chiede che un utente può avere solo una prenotazione per giornata
+        // quidni ora dovrò controllare che l'utente appunto non ha prenotato già per quella data
+        boolean utenteOccupato = prenotazioneRepository.existsByUtenteAndDataPrenotazione(
+                newPrenotazione.getUtente(),
+                newPrenotazione.getDataPrenotazione()
+        );
+
+        if(utenteOccupato){
+            log.error("impossibile salvare perchè l'utente {} ha già una prenotazione in carico per quella data {}",
+                    newPrenotazione.getUtente().getUsername(),
+                    newPrenotazione.getDataPrenotazione());
+            return;
+        }
+
+        // adesso che ho stabilito i criteri per i quali un utente può prenotare una postazione
+        //posso salvare nel DB
+
+        prenotazioneRepository.save(newPrenotazione);
+        log.info("la prenotazione: {} è stata salvata correttamente :)", newPrenotazione);
     }
-
-
-
-
 }
